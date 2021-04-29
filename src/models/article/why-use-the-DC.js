397be943526a9075a05129e890458cb633e4df0e
Chris-Bortel <bortelcc19@gmail.com>
2021-04-29 12:53:51 -0700

    This commit adds a data collection which will keep my models dry moving forward

---
+++ src/models/article/why-use-the-DC.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// TODO: Why do we not need this when using the data collection?
+// const Model = require('../data-collection.js');
+// const schema = require('./article-schema.js');
+
+// class Article extends Model {
+//   constructor() {
+//     super(schema);
+//   }
+// }
+
+// module.exports = Article;
--- src/models/data-collection.js
+++ src/models/data-collection.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// This takes in a schema and builds the REST methodology. Will be connected through the router
+
+class DataCollection {
+  constructor(model) {
+    this.model = model;
+  }
+
+  get(_id) {
+    if (_id) {
+      return this.model.findOne({ _id });
+    } else {
+      return this.model.find({});
+    }
+  }
+
+  create(record) {
+    let newRecord = this.model();
+    return newRecord.save();
+  }
+
+  update(_id, record) {
+    return this.model.findOneAndUpdate(_id, record, { new: true });
+  }
+
+  delete(_id) {
+    return this.model.findOneAndDelete(_id);
+  }
+}
+
+module.exports = DataCollection;
