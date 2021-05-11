130c64bd25040565395ecae0ecede2c3128dbc1b
Chris-Bortel <bortelcc19@gmail.com>
2021-05-11 16:48:20 -0700

    The commit adds a PUT test, and a broken DELETE test

--- __tests__/v1.test.js
+++ __tests__/v1.test.js
@@ -54,7 +54,31 @@ describe('v1 web server', () => {
     expect(response2.body.title).toEqual(response1.body.title);
   });
 
-  it('can update a record', async () => {});
+  it('can update a record', async () => {
+    const response1 = await mockRequest.post(`/api/v1/article`).send(data);
+    expect(response1.status).toBe(201);
+    const updatedData = { title: 'Story' };
+    const response2 = await mockRequest
+      .put(`/api/v1/article/${response1.body._id}`)
+      .send(updatedData);
+    expect(response2.status).toBe(201);
+    expect(response2.body._id).toEqual(response1.body._id);
+    expect(response2.body.title).toEqual('Story');
+  });
 
-  it('can delete a record', async () => {});
+  it('can delete a record', async () => {
+    console.log(data);
+    const response1 = await mockRequest.post(`api/v1/article`).send(data);
+    console.log(response1);
+    expect(response1.status).toBe(201);
+    // const response2 = await mockRequest
+    //   .delete(`api/v1/article/${response1.body._id}`)
+    //   .send('Deleted');
+    // console.log(response2);
+    // expect(response2.status).toBe(204);
+    // expect(response2.body).toBeFalsy();
+
+    // const getResponse = await mockRequest.get(`/api/v1/article`);
+    // expect(getResponse.body.length).toEqual(0);
+  });
 });
