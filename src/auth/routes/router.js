39136d3651e3c22d4e2d107289fcdd1b936a4d6a
Chris-Bortel <bortelcc19@gmail.com>
2021-05-17 13:07:14 -0700

    The commit adds a signup route

--- src/auth/routes/router.js
+++ src/auth/routes/router.js
@@ -2,25 +2,29 @@
 
 // Bring in dependencies
 const express = require('express');
+const router = express.Router();
 
 const basicAuth = require('../middleware/basicAuth.js');
 const bearerAuth = require('../middleware/bearerAuth.js');
 const acl = require('../middleware/acl.js');
-const 
-User = require('../models/userModel.js');
-
-const router = express.Router();
+const User = require('../models/userModel.js');
 
 // Build a '/signup' route
 router.post('/signup', async (req, res, next) => {
   try {
-    let obj = {
-      username: req.body.username,
-      password: req.body.password,
-      role: req.body.role,
-    }
-
-    // Create new instance of user
+    // bring in the user object from user-model
+    const user = new User(req.body);
+    const userRecord = await user.save();
+    const outputObject = {
+      user: userRecord,
+      token: userRecord.token,
+    };
+    // send the outputObject
+    res.status(201).json(outputObject);
+    // output an object with the user record and the attach token'
+  } catch (err) {
+    next(err.message);
   }
-})
+});
+
 // Build a '/signin' route
