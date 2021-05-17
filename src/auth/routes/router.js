1a1106cabc8e09df8004bcd07ecd39e68dc60a55
Chris-Bortel <bortelcc19@gmail.com>
2021-05-17 13:27:54 -0700

    This commit adds an authrouter that works locally but needs to be tested

--- src/auth/routes/router.js
+++ src/auth/routes/router.js
@@ -2,15 +2,15 @@
 
 // Bring in dependencies
 const express = require('express');
-const router = express.Router();
+const authRouter = express.Router();
 
 const basicAuth = require('../middleware/basicAuth.js');
 const bearerAuth = require('../middleware/bearerAuth.js');
-const acl = require('../middleware/acl.js');
+const userPermissions = require('../middleware/acl.js');
 const User = require('../models/userModel.js');
 
 // Build a '/signup' route
-router.post('/signup', async (req, res, next) => {
+authRouter.post('/signup', async (req, res, next) => {
   try {
     // bring in the user object from user-model
     const user = new User(req.body);
@@ -28,3 +28,33 @@ router.post('/signup', async (req, res, next) => {
 });
 
 // Build a '/signin' route
+authRouter.post('signin', basicAuth, (req, res, next) => {
+  // take the new user and send it
+  const user = {
+    user: req.user,
+    token: req.user.token,
+  };
+  res.status(200).json(user);
+});
+
+// Build the users route
+
+authRouter.get(
+  '/users',
+  bearer,
+  userPermissions('delete'),
+  async (req, res, next) => {
+    // find all users
+    const users = await User.find({});
+    const userList = users.map(user.username);
+    // return a list of all the users
+    res.status(200).json(userList);
+  }
+);
+
+// Build the secret route
+authRouter.get('/secret', bearerAuth, async (req, res, next) => {
+  res.status(200).send('This is secret');
+});
+
+module.exports = authRouter;
