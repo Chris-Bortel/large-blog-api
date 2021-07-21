1abb7b7e6e6c13e672424eb2e7968f61d075cb97
Chris-Bortel <bortelcc19@gmail.com>
2021-07-21 13:55:32 -0700

    I am still struggling with creating a user with a token attached. I am definately making this harder than it needs to be

--- __tests__/acl.test.js
+++ __tests__/acl.test.js
@@ -10,31 +10,34 @@ const jwt = require('jsonwebtoken');
 const mockRequest = supergoose(server);
 
 // TODO: I do not think that I need to bring in all of the middleware, I should just need the user model and acl.js.
-
+let token;
 let users = {
-  admin: { username: 'admin', password: 'password' },
+  admin: { username: 'admin', password: 'password', role: 'admin' },
 };
 
 // Pre-load our database with fake users
 beforeAll(async (done) => {
-  await new Users(users.admin).save();
+  const user = await new User(users.admin).save();
+  console.log(user);
+  // const token = jwt.sign(users, process.env.SECRET);
+  token = user.generateToken();
+  console.log(token);
   done();
 });
 
 describe('Access control tests', () => {
-  it('should respond with a 200 on a valid route', async () => {
-    const result = await mockRequest.get('/api/v1/article');
-    expect(result.status).toBe(200);
-  });
+  // it('should respond with a 200 on a valid route', async () => {
+  //   const result = await mockRequest.get('/api/v1/article');
+  //   expect(result.status).toBe(200);
+  // });
 
   it('should allow an admin to access the "/users" route', async () => {
-    const user = { username: 'admin' };
-    const token = jwt.sign(user, process.env.SECRET);
+    console.log(users.admin);
+    const token = jwt.sign(users, process.env.SECRET);
     console.log(token);
 
-    const aclResponse = await mockRequest
-      .get('/users')
-      .set('Authorization', `Bearer ${token}`);
+    const aclResponse = await mockRequest.get('/users');
+    // .set('Authorization', `Bearer ${token}`);
 
     console.log(aclResponse);
   });
